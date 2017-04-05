8e9c232dcf1dabc401a86ab64c0ea89431264a63
Serhii Marian <serhii_marian@epam.com>
2017-04-06 01:24:33 +0300

    Add single product properties

--- shop/src/app/app.component.ts
+++ shop/src/app/app.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
 
+enum Category {
+    vagetable = 1,
+    fruit = 2,
+    berry = 3
+}
+
+export class Product {
+  name: string;
+  description: string;
+  price: number;
+  category: Category;
+  isAvailable: boolean;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  product: Product = {
+    name: 'Watermelon',
+    description: 'Sweet and juice',
+    price: 650,
+    category: 3,
+    isAvailable: true
+  };
 }
